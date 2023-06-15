1e20b47fb957033c150d2be085f7dc392ae8256b
Aditya Varshney <aditya.varshneymail@gmail.com>
2023-06-15 20:26:51 +0530

    initial commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="w-screen ">
+      <div className="w-full bg-gray-400">
+<a href="" className="text-7xl"> Hello </a>
+
+      </div>
+    </div>
+  )
+}
+
+export default Navbar;
