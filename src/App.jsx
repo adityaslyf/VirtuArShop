6923136a09129f4db259f2c187804834e4f32a1c
Aditya Varshney <aditya.varshneymail@gmail.com>
2023-06-18 00:46:27 +0530

    adding components

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,27 @@
-import "./App.css";
+import Carousel from "./components/ carousel";
 import Navbar from "./components/Navbar";
 
+import Fwatch from "./components/Fwatch";
+import Fglasses from "./components/Fglasses";
+import Shoes from "./components/shoes.jsx";
+import Table from "./components/table.jsx";
+import Iphone from "./components/iphone.jsx";
 
 function App() {
-
   return (
     <>
-     <Navbar /> 
+      <Navbar />
 
-     
+      <Carousel />
+      <div className="flex">
+      <Fwatch />
+ <Fglasses />
+ <Shoes />
+ <Table />
+ <Iphone />
+      </div>
     </>
   );
-}
+};
 
 export default App;
